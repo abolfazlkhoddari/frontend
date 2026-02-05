de9dd369d4baf7bf93591bbd6bb2bb3d3cb6fe30
abolfazl-khoddary <asbrrrrrr@gmail.com>
2026-02-05 22:49:48 +0330

    تکمیل قسمت crouser slider  سایت

--- froshghah/js/home.js
+++ froshghah/js/home.js
@@ -9,3 +9,15 @@ menuBtn.addEventListener('click' , ()=>{
 closeBtn.addEventListener('click' , ()=>{
     headerMenu.classList.remove('showMenu')
 })
+//carouser slider
+
+
+const slider = document.getElementById('slider')
+function activate(e){
+    const items = document.querySelectorAll('.sliderItem')
+    e.target.matches('.next') && slider.append(items[0])
+    e.target.matches('.prev') && slider.prepend(items[items.length - 1]);
+
+
+}
+document.addEventListener('click', activate)
