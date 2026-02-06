3bbd943833af166ebf45267a889a0794e7915088
abolfazl-khoddary <asbrrrrrr@gmail.com>
2026-02-07 00:24:51 +0330

    ساخت قسمت carouser section  چه زهر ماری بود خدا برا کسی نخواد

--- froshghah/js/home.js
+++ froshghah/js/home.js
@@ -21,3 +21,65 @@ function activate(e){
 
 }
 document.addEventListener('click', activate)
+
+//carouser section
+let csSlider = document.querySelectorAll('.crouser .slider img')
+let csLink = document.querySelectorAll(".crouserSection .crouser .nav-link")
+let csBar = document.querySelectorAll(".crouser .overlays .bar")
+let maxZindex = csSlider.length;
+let easeInOutQuart = "cubic-bezier(0.77, 0, 0.175, 1)";
+csSlider[0].classList.add('active')
+csLink[0].classList.add('showli')
+
+csLink.forEach((link , index)  => {
+    csBar[index].style.zIndex = `${maxZindex - index}`
+
+    //link chenenge
+    link.addEventListener('click' , ()=>{
+        csLink.forEach(links => {
+            links.classList.remove('showli')
+        });
+        link.classList.add('showli')
+    
+        //slider chenge
+        let curentSlide = document.querySelector(".crouser .slider img.active")
+        let slideFideOute = curentSlide.animate(
+            [
+                {transform:"translateX(0)" , opacity:1},
+                {transform:'translateX(5%)' , opacity:0}],
+        {
+            duration:600,
+            easing: "ease-in",
+            fill: "forwards"
+        })
+        slideFideOute.onfinish = ()=>{
+            csSlider.forEach(slide =>{ slide.classList.remove('active')})
+                let activeSlide = csSlider[index];
+                activeSlide.classList.add('active')
+                activeSlide.animate(
+                    [ {transform: 'translateX(-5%)' , opacity:0} ,
+                    {transform : 'translateX(0)' , opacity:1 }],
+                    {
+                        duration: 600, easing: "ease-out", fill: "forwards"
+                    }
+                )
+
+            }
+            //overlays
+            maxZindex += 1
+            let activeBar = csBar[index]
+            activeBar.style.zIndex =`${maxZindex}`
+            activeBar.animate(
+                [
+                    {transform:'scaleX(0)'},
+                    {transform:'scaleX(1)'}
+                ],
+                {
+                    duration:1200,
+                    fill:'forwards',   
+                    easing: easeInOutQuart                  
+                }
+            )
+
+    })
+})
